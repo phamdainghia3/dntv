5f3b3173dc8405297a20c42fdd0cf98dcfe2ec0e
phamdainghia3 <phamdainghia3@gmail.com>
2025-01-26 21:30:44 +0700

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -31,7 +31,8 @@ module.exports = {
 	transform: async (config, path) => {
 		// Get the current date and time in the format m/d/Y g:i a
 		const currentDate = new Date()
-		const formattedDate = `${currentDate.getMonth() + 1}/${currentDate.getDate()}/${currentDate.getFullYear()} ${currentDate.getHours() % 12 || 12}:${currentDate.getMinutes().toString().padStart(2, '0')} ${currentDate.getHours() >= 12 ? 'PM' : 'AM'}`
+		//const formattedDate = `${currentDate.getMonth() + 1}/${currentDate.getDate()}/${currentDate.getFullYear()} ${currentDate.getHours() % 12 || 12}:${currentDate.getMinutes().toString().padStart(2, '0')} ${currentDate.getHours() >= 12 ? 'PM' : 'AM'}`
+		const formattedDate = `${currentDate.getDate().toString().padStart(2, '0')}/${(currentDate.getMonth() + 1).toString().padStart(2, '0')}/${currentDate.getFullYear()} ${currentDate.getHours() % 12 || 12}:${currentDate.getMinutes().toString().padStart(2, '0')} ${currentDate.getHours() >= 12 ? 'PM' : 'AM'}`;
 
 		// Define paths to assign low priority
 		const lowPriorityPaths = ['/contact', '/login', '/sign-up'] // Check paths without the trailing slashes
